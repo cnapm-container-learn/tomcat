63330dd426efae751f44793155289c2f8b756c07
wdxxs2z <243219650@qq.com>
2015-12-29 17:49:54 +0800

    Update pinpoint.

--- pinpoint/set.sh
+++ pinpoint/set.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-export profiler.collector.ip=${PINPOINT_PROFILER_COLLECTOR_IP}
-
-export JAVA_OPTS="-javaagent:/pinpoint/pinpoint-bootstrap-${PINPOINT_AGENT_VERSION}.jar -Dpinpoint.agentId=${PINPOINT_AGENT_ID} -Dpinpoint.applicationName=${PINPOINT_APP_NAME}"
+sed -i "s#^profiler.collector.ip=.*#profiler.collector.ip=${PINPOINT_PROFILER_COLLECTOR_IP}#g" /pinpoint/pinpoint-agent-${PINPOINT_AGENT_VERSIO
+N}/pinpoint.config
