072108e1e5ce46032f627ea2b40b3aa2d0c337b8
wdxxs2z <243219650@qq.com>
2015-12-31 17:04:57 +0800

    Fix pinpoint set.sh bug.

--- pinpoint/set.sh
+++ pinpoint/set.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sed -i "s#^profiler.collector.ip=.*#profiler.collector.ip=${PINPOINT_PROFILER_COLLECTOR_IP}#g" /pinpoint/pinpoint-agent-${PINPOINT_AGENT_VERSIO
-N}/pinpoint.config
+sed -i "s#^profiler.collector.ip=.*#profiler.collector.ip=${PINPOINT_PROFILER_COLLECTOR_IP}#g" /pinpoint/pinpoint-agent-${PINPOINT_AGENT_VERSION}/pinpoint.config
